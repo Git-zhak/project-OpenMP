50b19536c67030ec1e00811f71ddb1bece4ad629
Yitzhak <yitzhak_en@outlook.com>
2026-05-15 19:39:02 -0600

    bash con pruebas dynamic y guided

---
+++ benchmark.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Asegurarse de usar el máximo de hilos
+export OMP_NUM_THREADS=$(nproc)
+echo "Evaluando con $OMP_NUM_THREADS hilos físicos/lógicos..."
+echo "--------------------------------------------------------"
+
+# Lista de configuraciones a probar
+configs=("static" "dynamic,1" "dynamic,10" "dynamic,64" "dynamic,256" "guided,1" "guided,10" "guided,64" "guided,256")
+
+for conf in "${configs[@]}"; do
+    export OMP_SCHEDULE="$conf"
+    echo "Prueba con OMP_SCHEDULE=$conf"
+    
+    # Ejecutamos el programa, pero filtramos la salida para ver solo la Tarea A
+    ./fractal_omp | grep "Tarea A"
+    echo "---"
+done
